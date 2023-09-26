d0b7e518dd196c0af4b6b7b573f711427dd04c10
kayladong229 <105991864+kayladong229@users.noreply.github.com>
2023-09-26 13:12:20 -0700

    Created initial files and added math functions

---
+++ script.js
@@ -0,0 +1,28 @@
+let num1;
+let num2;
+let operator;
+
+const add = (num1, num2) => num1 + num2;
+const subtract = (num1, num2) => num1 - num2;
+const multiply = (num1, num2) => num1 * num2;
+const divide = (num1, num2) => num1 / num2;
+
+const operate = (num1, operator, num2) => {
+  let result;
+  switch (operator) {
+    case '+':
+      result = add(num1, num2);
+      break;
+    case "-":
+      result = subtract(num1, num2);
+      break;
+    case "*":
+      result = multiply(num1, num2);
+      break;
+    case "/":
+      result = divide(num1, num2);
+      break;
+  }
+
+  return result;
+};
